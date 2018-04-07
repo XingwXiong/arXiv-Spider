3916283ef4983078d65b66b373b33bead7060b5a
xingw-xiong <xingw.xiong@gmail.com>
2018-04-07 21:25:18 +0800

    update main.py

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # arXiv-Spider
-a Web Spider - to crawl the latest papers on arXiv
+a Web Spider - to crawl papers on arXiv by specify the submission  date
 
+You can change the configuration in main.py
+
+With the default configuration, it can crawl the papers with submission dates from January 2015 to March 2018
--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from spider import Spider
 import threading
 
 # the patten to extract file id from the HTML context
-s_pattern_pdf = r'<span class="list-identifier">.*?\[<a href="/pdf/(.*?)" title="Download PDF">pdf</a>'
+S_PATTERN_PDF = r'<span class="list-identifier">.*?\[<a href="/pdf/(.*?)" title="Download PDF">pdf</a>'
 # the pattern to extract total entries number
-s_pattern_entries = r'[ total of (.*?) entries:'
-base_url = 'https://arxiv.org'
-# skip means the offset, show means how many entries per page
-page_url = '/list/cs/1801'  # /list/cs/1801?skip=0&show=1000
-save_dir = './pdfs/'
+S_PATTERN_EMTRIES = r'\[ total of (.*?) entries:'
+BASE_URL = 'https://arxiv.org'
+# the path to save the crawled files
+SAVE_DIR = './pdfs/'
+# It's a list of the file names to be crawled(Excluding file suffixes)
 task_list = []
 task_list_lock = threading.Lock()
-worker_num = 50
+spider = Spider(BASE_URL, SAVE_DIR)
+WORKER_NUM = 50
+BEGIN_YEAR = 15
+BEGIN_MONTH = 1
+END_YEAR = 18
+END_MONTH = 3
 
 
 class Worker(threading.Thread):
@@ -30,37 +35,58 @@ class Worker(threading.Thread):
             file_id = task_list[0]
             task_list.pop(0)
             task_list_lock.release()
-            file_name = file_id + ".pdf"
-            file_url = '/pdf/' + file_id
+            file_name = "%s.pdf" % file_id
+            file_url = '/pdf/%s' % file_id
             spider.get_file(file_name, file_url)
 
 
-if __name__ == '__main__':
-    year = 18
-    month = 03
-    spider = Spider(base_url, save_dir)
-
-    while year > 15:
-        # /list/cs/1801?skip=0&show=1000
-        page_url = '/list/cs/%2d%2d?skip=0&show=1000' % (year, month)
+def download(_year, _month):
+    global task_list
+    skip = 0
+    show = 5
+    page_url = '/list/cs/%2d%02d' % (_year, _month)
+    page = spider.get_page(page_url)
+    pattern_total = re.compile(S_PATTERN_EMTRIES, re.S)
+    total_obj = re.search(pattern_total, page)
+    if not total_obj:
+        print('total match failed:[%s]' % S_PATTERN_EMTRIES)
+        return False
+    total_num = int(total_obj.group(1))
+    print("page_url:%s\ntotal_num:%d" % (page_url, total_num))
+    while skip < total_num:
+        #  skip means the offset, show means how many entries per page
+        #  e.g.:/list/cs/1801?skip=0&show=1000
+        page_url = '/list/cs/%02d%02d?skip=%d&show=%d' % (_year, _month, skip, show)
         page = spider.get_page(page_url)
-        pattern_pdf = re.compile(s_pattern_pdf, re.S)
+        pattern_pdf = re.compile(S_PATTERN_PDF, re.S)
         # get all the file id
         task_list = re.findall(pattern_pdf, page)
+        task_list_size = len(task_list)
         workers = []
         if task_list:
-            for index in range(1, worker_num):
+            for index in range(1, WORKER_NUM):
                 worker = Worker(index, "Thread-%d" % index)
                 worker.start()
                 workers.append(worker)
             for worker in workers:
                 worker.join()
         else:
-            print('match failed:[%s]' % s_pattern_pdf)
-            break
-        if month == 1:
-            month = 12
-            year = year - 1
+            print('files match failed:[%s]' % S_PATTERN_PDF)
+            return False
+        print("Ranging from %d to %d was downloaded!" % (skip + 1, skip + task_list_size))
+        skip = skip + show
+    return True
+
+
+if __name__ == '__main__':
+    # Init date
+    _year = END_YEAR
+    _month = END_MONTH
+    while _year > BEGIN_YEAR or (_year == BEGIN_YEAR and _month > BEGIN_MONTH):
+        download(_year, _month)
+        if _month == 1:
+            _month = 12
+            _year = _year - 1
         else:
-            month = month - 1
+            _month = _month - 1
     print("All tasks finished! Exiting Main Thread")
